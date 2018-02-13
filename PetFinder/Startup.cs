b907b1026a53459b8e6d59a7d820011fa7d40dbd
Wade Carlson <wcarlsondev@gmail.com>
2018-02-13 13:31:30 -0600

    basic file setup

---
+++ PetFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetFinder.Startup))]
+namespace PetFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
