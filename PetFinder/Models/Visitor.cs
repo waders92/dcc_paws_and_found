9fcff46f98771c22b9f4f45979da6b5486a737fa
Linda Berez <lberez@gmail.com>
2018-02-13 13:45:00 -0600

    Push Pull Test

---
+++ PetFinder/Models/Visitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetFinder.Models
+{
+    public class Visitor
+    {
+        [Key]
+        public int VisitorID { get; set; }
+        public string ScreenName { get; set; }
+        public string UserID { get; set; }
+    }
+}
